e47c5b6519daf8d532e218179e2d1a3ee13e982c
Oscar Andersson <oscar.andersson.200@gmail.com>
2021-08-03 15:22:19 +0200

    RB USB init.

--- lib/rbdj/djusb.go/djusb.go
+++ lib/rbdj/djusb.go/djusb.go
@@ -1,1 +1,16 @@
-package djusbgo
+package djusb
+
+import (
+	"loopbox/utils/logger"
+	"os"
+	"path/filepath"
+)
+
+func Scan(root string) {
+	if fErr := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		logger.Info("[djusb] opened " + root)
+		return nil
+	}); fErr != nil {
+		logger.Error("[djusb] Error opening usb: " + fErr.Error())
+	}
+}
