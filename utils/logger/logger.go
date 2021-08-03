5ab98553d318a1f56daad04081854a66d8ffbb77
Oscar Andersson <oscar.andersson.200@gmail.com>
2021-08-03 14:13:19 +0200

    Logger with env vars.

--- utils/logger/logger.go
+++ utils/logger/logger.go
@@ -1,22 +1,45 @@
 package logger
 
 import (
+	"fmt"
 	"log"
+	"os"
+
+	"github.com/fatih/color"
 )
 
 func Info(msg string) {
-
+	if fmt.Sprint(os.Getenv("LOG_INFO")) == "true" {
+		flag := "[info]"
+		if fmt.Sprint(os.Getenv("COLORED_OUTPUT")) == "true" {
+			msg = color.New(color.FgHiCyan).Sprint(msg)
+		}
+		out(flag + msg)
+	}
 }
 
 func Warn(msg string) {
-
+	if fmt.Sprint(os.Getenv("LOG_WARN")) == "true" {
+		flag := "[warn]"
+		if fmt.Sprint(os.Getenv("COLORED_OUTPUT")) == "true" {
+			msg = color.New(color.FgHiYellow).Sprint(msg)
+		}
+		out(flag + msg)
+	}
 }
 
 func Error(msg string) {
-
+	if fmt.Sprint(os.Getenv("LOG_ERROR")) == "true" {
+		flag := "[error]"
+		if fmt.Sprint(os.Getenv("COLORED_OUTPUT")) == "true" {
+			msg = color.New(color.FgHiRed).Sprint(msg)
+		}
+		out(flag + msg)
+	}
 }
 
-func Out(s string) {
-	log.Println(s)
-	return
+func out(in string) {
+	pid := fmt.Sprint(os.Getpid())
+	appName := os.Getenv("APP_NAME")
+	log.Println("(" + pid + ")[" + appName + "]" + in)
 }
